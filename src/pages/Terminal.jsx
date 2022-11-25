fa51c37feda647de04669056450eb051053d2e8b
dayani <www.dharmikdayani0959@gmail.com>
2022-11-25 20:29:50 +0530

    chang

--- src/pages/Terminal.jsx
+++ src/pages/Terminal.jsx
@@ -199,17 +199,61 @@ const Terminal = () => {
             </div>
           )}
 
-          <div className="bg-white  dark:text-gray-200 dark:bg-[#2d313d]    px-5 py-2 mx-3 mt-3">
-            <p className="font-bold">Time in force</p>
-            <p className=" text-sm text-[#606266] py-2">
-              Time in minutes before Limit order expires (5-5000 min)
-            </p>
-            <input
-              type="number"
-              name=""
-              placeholder={0.0}
-              className="w-full my-2 dark:text-gray-200 outline-none  rounded bg-transparent border-b "
-            />
+         
+          <div className="bg-white  dark:text-gray-200 dark:bg-[#2d313d]   px-5 py-5 mx-3 mt-3">
+            <p className="font-bold ">Portfolio size</p>
+            <div className="py-3 ppx-2">
+              <button className=" bg-[#598fff] border-[#3c60c3] border  text-black dark:text-white text-sm  rounded  px-2 py-2">
+                Percents
+              </button>
+              <button className=" false border-[#3c60c3] border  text-black dark:text-white text-sm  rounded  px-2 py-2 ml-3">
+                Fixed Amount
+              </button>
+              <div className="flex gap-5 flex-wrap justify-between ">
+                <div>
+                  <p className=" text-sm text-[#606266] py-2">Portfolio size</p>
+                  <div className="flex ">
+                    <div className="flex flex-col">
+                      <input
+                        type="number"
+                        name=""
+                        placeholder="0%"
+                        className="w-full my-2 dark:text-gray-200 outline-none  rounded bg-transparent border-b "
+                        defaultValue={0}
+                      />
+                      <div className="flex gap-2 cursor-pointer">
+                        <p className="border text-xs rounded-full p-1 border-[#e04caf] hover:bg-[#e04caf]">
+                          1%
+                        </p>
+                        <p className="border text-xs rounded-full p-1 border-[#e04caf] hover:bg-[#e04caf]">
+                          3%
+                        </p>
+                        <p className="border text-xs rounded-full p-1 border-[#e04caf] hover:bg-[#e04caf]">
+                          5%
+                        </p>
+                        <p className="border text-xs rounded-full p-1 border-[#e04caf] hover:bg-[#e04caf]">
+                          10%
+                        </p>
+                        <p className="border text-xs rounded-full p-1 border-[#e04caf] hover:bg-[#e04caf]">
+                          20%
+                        </p>
+                      </div>
+                    </div>
+                    <span>%</span>
+                  </div>
+                </div>
+                <div className="flex flex-col gap-2 justify-start items-start">
+                  <p className="text-sm text-[#606266] py-2">
+                    Amount in quote currency
+                  </p>
+                  <input
+                    type="number"
+                    className="bg-[#606266] dark:text-white  outline-none px-1 rounded"
+                    placeholder={0.0}
+                  />
+                </div>
+              </div>
+            </div>
           </div>
           <div className="bg-white  dark:text-gray-200 dark:bg-[#2d313d]   px-5 py-2 mx-3 mt-3">
             <div className="flex gap-2">
@@ -221,6 +265,91 @@ const Terminal = () => {
               Stop Loss are disabled.
             </p>
           </div>
+          <div className="bg-white  dark:text-gray-200 dark:bg-[#2d313d]   px-5 py-2 mx-3 mt-3">
+            <div className="list-container flex flex-col gap-2">
+              <div className="flex items-center gap-2">
+                <input
+                  type="checkbox"
+                  className="w-5"
+                  defaultValue="Take Profit 1"
+                />
+                <span className="not-checked-item dark:text-white py-2 text-lg font-bold">
+                  Take Profit 1
+                </span>
+              </div>
+              <div className="flex items-center gap-2">
+                <input
+                  type="checkbox"
+                  className="w-5"
+                  defaultValue="Take Profit 2"
+                />
+                <span className="not-checked-item dark:text-white py-2 text-lg font-bold">
+                  Take Profit 2
+                </span>
+              </div>
+              <div className="flex items-center gap-2">
+                <input
+                  type="checkbox"
+                  className="w-5"
+                  defaultValue="Take Profit 3"
+                />
+                <span className="not-checked-item dark:text-white py-2 text-lg font-bold">
+                  Take Profit 3
+                </span>
+              </div>
+              <div className="flex items-center gap-2">
+                <input
+                  type="checkbox"
+                  className="w-5"
+                  defaultValue="Take Profit 4"
+                />
+                <span className="not-checked-item dark:text-white py-2 text-lg font-bold">
+                  Take Profit 4
+                </span>
+              </div>
+              <div className="flex items-center gap-2">
+                <input
+                  type="checkbox"
+                  className="w-5"
+                  defaultValue="Take Profit 5"
+                />
+                <span className="not-checked-item dark:text-white py-2 text-lg font-bold">
+                  Take Profit 5
+                </span>
+              </div>
+              <div className="flex items-center gap-2">
+                <input
+                  type="checkbox"
+                  className="w-5"
+                  defaultValue="Take Profit 6"
+                />
+                <span className="not-checked-item dark:text-white py-2 text-lg font-bold">
+                  Take Profit 6
+                </span>
+              </div>
+              <div>
+                <div className="flex items-center gap-2">
+                  <input type="checkbox" name="" id="" className="w-5" />
+                  <p className="font-bold">Stop Loss</p>
+                </div>
+              </div>
+              <div>
+                <div className="flex items-center gap-2">
+                  <input type="checkbox" name="" id="" className="w-5" />
+                  <p className="font-bold">Move Stop Loss to breakeven</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="bg-white  dark:text-gray-200 dark:bg-[#2d313d]  px-5 py-2 mx-3 mt-3">
+            <div className="flex gap-2">
+              <input type="checkbox" name="" id="" className="w-4" />
+              <p className="font-bold">Trailing Stop</p>
+            </div>
+          </div>
+          <button className=" flex items-stretch hover:bg-[#598fff] mt-3 border-[#598fff] border px-12 mx-auto py-4 text-black dark:text-white font-bold  rounded-2xl  btn">
+            Create a Strategy
+          </button>
         </div>
       </div>
     </>
